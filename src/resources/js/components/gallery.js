d1bccfa5d4a16e4f796009aef7bdba05c6697e35
osenbal <enbal.blogs.215@gmail.com>
2022-02-23 09:26:25 +0700

    add icon file type gallery

--- src/resources/js/components/gallery.js
+++ src/resources/js/components/gallery.js
@@ -19,7 +19,7 @@ $(document).ready(() => {
 
             if ($(this).parent().find(`.frame_modal_body_video`).children().length > 0) {
             } else {
-                $(this).parent().find(`.frame_modal_body_video`).append(`<iframe loading="lazy" class="embed-responsive-this skeleton" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video" width="100%" height="300" src="https://www.youtube.com/embed/${srcVideo}"></iframe>`);
+                $(this).parent().find(`.frame_modal_body_video`).append(`<iframe loading="lazy" class="embed-responsive-this skeleton" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video" width="100%" height="480" src="https://www.youtube.com/embed/${srcVideo}"></iframe>`);
             }
         }
     })
