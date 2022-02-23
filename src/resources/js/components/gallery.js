df5b93783c53e9252a1caeccdc3d5dc674c77cf1
osenbal <enbal.blogs.215@gmail.com>
2022-02-23 09:52:40 +0700

    fix video stop on modal closed

--- src/resources/js/components/gallery.js
+++ src/resources/js/components/gallery.js
@@ -2,24 +2,24 @@ $(document).ready(() => {
 
     $('.modal').on('hidden.bs.modal', function (e) {
         // do something...
-        $('.modal #video').attr("src", $(".modal  #video").attr("src"));
+        $(this).parent().find('#video').attr("src", $(this).parent().find("#video").attr("src"));
     });
 
     $('.thumbnail_gallery').on('click', function (e) {
-        console.log('test')
         if (this.getAttribute("type-file") === "photo") {
             let srcPhoto = this.getAttribute("srcModal");
 
             $(this).parent().find('.frame_modal_gallery img').attr('src', '');
-            $(this).parent().find('.frame_modal_gallery img').attr('src', `public/uploads/${srcPhoto}`);
+            $(this).parent().find('.frame_modal_gallery img').attr('src', `public/uploads/${$(this).attr("srcModal")}`);
 
         } else if (this.getAttribute("type-file") === "video") {
-            e.preventDefault();
-            let srcVideo = this.getAttribute("srcModal");
+            // e.preventDefault();
+            // let srcVideo = $(this).attr("srcModal");
 
             if ($(this).parent().find(`.frame_modal_body_video`).children().length > 0) {
+                // return false;
             } else {
-                $(this).parent().find(`.frame_modal_body_video`).append(`<iframe loading="lazy" class="embed-responsive-this skeleton" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video" width="100%" height="360" src="https://www.youtube.com/embed/${srcVideo}"></iframe>`);
+                $(this).parent().find(`.frame_modal_body_video`).append(`<iframe loading="lazy" class="embed-responsive-this skeleton" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video" width="100%" height="360" src="https://www.youtube.com/embed/${$(this).attr("srcModal")}"></iframe>`);
             }
         }
     })
