329135556034c87737ebc8424fc5384147d829a6
osenbal <enbal.blogs.215@gmail.com>
2022-02-23 09:11:50 +0700

    fix logic gallery modal view

--- src/resources/js/components/gallery.js
+++ src/resources/js/components/gallery.js
@@ -5,27 +5,24 @@ $(document).ready(() => {
         $('.modal #video').attr("src", $(".modal  #video").attr("src"));
     });
 
-    $('.thumbnail_gallery').each((index, item) => {
+    $('.thumbnail_gallery').on('click', function (e) {
+        console.log('test')
+        if (this.getAttribute("type-file") === "photo") {
+            let srcPhoto = this.getAttribute("srcModal");
 
-        item.addEventListener('click', (e) => {
-            if (item.getAttribute("type-file") === "photo") {
-                let srcPhoto = item.getAttribute("srcModal");
-                // $('.wrap_modal_body').html('');
-                // $('.wrap_modal_body').append(galleryIframeVideo(`public/uploads/${srcPhoto}`));
-                $('.frame_modal_gallery img').attr('src', '');
-                $('.frame_modal_gallery img').attr('src', `public/uploads/${srcPhoto}`);
+            $(this).parent().find('.frame_modal_gallery img').attr('src', '');
+            $(this).parent().find('.frame_modal_gallery img').attr('src', `public/uploads/${srcPhoto}`);
 
-            } else if (item.getAttribute("type-file") === "video") {
-                e.preventDefault();
-                let srcVideo = item.getAttribute("srcModal");
-                $('.frame_modal_body_video').html('');
-                $('.frame_modal_body_video').append(`<iframe loading="lazy" class="embed-responsive-item skeleton" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video" width="100%" height="300" src="https://www.youtube.com/embed/${srcVideo}">
-                </iframe>`);
+        } else if (this.getAttribute("type-file") === "video") {
+            e.preventDefault();
+            let srcVideo = this.getAttribute("srcModal");
 
-                // $('.frame_modal_gallery iframe').attr('src', '');
-                // $('.frame_modal_gallery iframe').attr('src', `https://www.youtube.com/embed/${item.getAttribute("srcModal")}`)
+            if ($(this).parent().find(`.frame_modal_body_video`).children().length > 0) {
+            } else {
+                $(this).parent().find(`.frame_modal_body_video`).append(`<iframe loading="lazy" class="embed-responsive-this skeleton" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video" width="100%" height="300" src="https://www.youtube.com/embed/${srcVideo}"></iframe>`);
             }
-        });
-
+        }
     })
+
+
 });
