768793c99de2a490d9b8bf75c0797ef9d75c0ddd
iyan <akiyan2002@gmail.com>
2022-03-18 01:19:39 +0700

    delete unnecessary middleware

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -62,7 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'customer' => \App\Http\Middleware\CustomerMiddleware::class,
         'is_admin' => \App\Http\Middleware\IsAdmin::class
     ];
